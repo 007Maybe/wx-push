ff6d349994feda40484e1bd5124cf432d6f45294
007Maybe <76901219+007Maybe@users.noreply.github.com>
2023-07-06 23:13:49 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wx0bf2dad299aaa535",
   appsecret: "b8f3cf40169f74c7fb945242dbc560b6",
   template_id: "jsNe-djzdUsPYIIO1rfzxBzeYmplzqs_wptjwn4F9AE",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  touser: "oCyDc6ZUWda02gR8_Gb5JFRl3yiI",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "石家庄",
